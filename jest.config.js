6c43962f7c3fdcabd64fdf73621c1d0a2ae3d929
Ítalo Andrade <italodeandra@gmail.com>
2021-04-29 21:09:39 +0200

    fix jest transform

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
     "<rootDir>/dist/",
   ],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
 }
