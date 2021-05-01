6827e31bd0d86013aa4b42ce5947932fdeb30d52
Ítalo Andrade <italodeandra@gmail.com>
2021-05-02 01:01:45 +0200

    add Link demo

---
+++ lib/components/Link/Link.demo.tsx
@@ -0,0 +1,40 @@
+import { Link } from "./Link"
+import { useDocumentation } from "../../hooks"
+
+export const LinkDemo = () => {
+  const { children, icon, ...props } = useDocumentation(
+    {
+      children: {
+        description: "Content of the link.",
+        value: "Link",
+      },
+      color: {
+        description: "The color of the link.",
+        options: [
+          "primary",
+          "secondary",
+          "error",
+          "textPrimary",
+          "textSecondary",
+        ],
+        value: "primary",
+      },
+      href: {
+        description: "URL of the link.",
+        value: "https://majapi.com",
+      },
+    },
+    ({ children, color, href }) => `<Link${
+      color !== "primary"
+        ? `
+  color="${color}"`
+        : ""
+    }
+  href="${href}"
+>
+  ${children}
+</Link>`
+  )
+
+  return <Link {...props}>{children}</Link>
+}
