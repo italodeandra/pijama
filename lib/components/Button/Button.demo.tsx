189f04b9cb60381a34d8515dfc230f97dd1df83c
Ítalo Andrade <italodeandra@gmail.com>
2021-05-01 14:00:41 +0200

    add all the properties of Button to the demo

--- lib/components/Button/Button.demo.tsx
+++ lib/components/Button/Button.demo.tsx
@@ -1,8 +1,10 @@
 import { Button } from "./Button"
+import homeIcon from "@iconify/icons-heroicons-outline/home"
+import Icon from "@iconify/react"
 import { useDocumentation } from "../../hooks"
 
 export const ButtonDemo = () => {
-  const props = useDocumentation(
+  const { children, icon, ...props } = useDocumentation(
     {
       children: {
         description: "The label or content.",
@@ -11,19 +13,68 @@ export const ButtonDemo = () => {
       color: {
         description:
           "The color of the background when contained, color of the border when outlined, or color of the text.",
+        options: [
+          "primary",
+          "secondary",
+          "error",
+          "textPrimary",
+          "textSecondary",
+        ],
         value: "primary",
       },
       href: {
         description: "Turn the button into a link.",
-        value: "",
+        value: undefined,
+      },
+      icon: {
+        description: "If should be shown with a square style for icons.",
+        options: [true, false],
+        value: false,
+      },
+      onClick: {
+        description: "Click event handler.",
+        readOnly: true,
+        value: "MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>",
+      },
+      size: {
+        description: "Size of the button.",
+        options: ["small", "normal"],
+        value: "normal",
+      },
+      type: {
+        description: "Type of the button.",
+        options: ["button", "submit", "reset"],
+        value: "button",
+      },
+      variant: {
+        description: "The variant style.",
+        options: ["contained", "outlined", "text"],
+        value: "contained",
       },
     },
-    ({ children, color, href }) => `<Button color="${color}"${
-      href ? ` href="${href}"` : ""
-    }>
-  ${children}
+    ({ children, color, href, icon, size, type, variant }) => `<Button
+  color="${color}"${
+      href
+        ? `
+  href="${href}"`
+        : ""
+    }${
+      icon
+        ? `
+  icon`
+        : ""
+    }
+  size="${size}"
+  type="${type}"
+  variant="${variant}"
+>
+  ${!icon ? children : `<Icon icon={homeIcon} />`}
 </Button>`
   )
 
-  return <Button {...props} />
+  return (
+    <Button icon={icon} {...props}>
+      {!icon ? children : <Icon icon={homeIcon} />}
+    </Button>
+  )
 }
