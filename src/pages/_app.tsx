0e7d588967d45b71c8e9ffbb17c81b7dde63e8a4
Ítalo Andrade <italodeandra@gmail.com>
2021-05-02 04:46:59 +0200

    add icons and seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,49 @@ import { QueryClientProvider } from "react-query"
 import { ThemeProvider } from "../../lib"
 import useQueryClientRef from "../queryClient"
 import Head from "next/head"
+import { DefaultSeo } from "next-seo"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const queryClientRef = useQueryClientRef()
 
   return (
     <ThemeProvider>
+      <DefaultSeo
+        description="For building apps."
+        openGraph={{
+          images: [
+            {
+              url: "/icons/android-chrome-512x512.png",
+              height: 512,
+              width: 512,
+              alt: "Majapinho",
+            },
+          ],
+        }}
+        title="Pijama"
+      />
       <Head>
         <title>Pijama</title>
+        <meta content="initial-scale=1, width=device-width" name="viewport" />
+        <link
+          href={"/icons/apple-touch-icon.png"}
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href={"/icons/favicon-32x32.png"}
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href={"/icons/favicon-16x16.png"}
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href={"/favicon.ico"} rel="icon" />
+        <link href={"/site.webmanifest"} rel="manifest" />
       </Head>
 
       <QueryClientProvider client={queryClientRef.current}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from "next/document"
 import React from "react"
+import { defaultTheme } from "../../lib"
 
 export default class MyDocument extends Document {
   render() {
     // noinspection HtmlRequiredTitleElement
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* PWA primary color */}
+          <meta content={defaultTheme.color.primary} name="theme-color" />
+        </Head>
         <body>
           <Main />
           <NextScript />
