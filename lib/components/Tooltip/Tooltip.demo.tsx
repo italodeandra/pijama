83f4116c08d9bf5c055d2ac0983469217343f8a0
Ítalo Andrade <italodeandra@gmail.com>
2021-05-02 02:10:53 +0200

    add Tooltip demo

---
+++ lib/components/Tooltip/Tooltip.demo.tsx
@@ -0,0 +1,28 @@
+import { Tooltip } from "./Tooltip"
+import { useDocumentation } from "../../hooks"
+import { Box } from "../Box/Box"
+
+export const TooltipDemo = () => {
+  useDocumentation(
+    {},
+    () => `<Tooltip title="This is the tooltip">
+  <span>Hover me</span>
+</Tooltip>`
+  )
+
+  return (
+    <Box
+      sh={(theme) => ({
+        display: "flex",
+        height: `calc(100vh - ${theme.spacing(12)})`,
+      })}
+    >
+      <Tooltip title={"This is the tooltip"}>
+        <Box sh={{ mb: "auto" }}>Hover me</Box>
+      </Tooltip>
+      <Tooltip title={"This is the tooltip"}>
+        <Box sh={{ ml: "auto", mt: "auto" }}>Hover me</Box>
+      </Tooltip>
+    </Box>
+  )
+}
