17c6b60338335d4b27925091dee50255a7dc3ad1
Ítalo Andrade <italodeandra@gmail.com>
2021-05-10 23:25:00 +0200

    improvement

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -1,9 +1,17 @@
+import { Box } from "../../../lib"
 import darcula from "react-syntax-highlighter/dist/cjs/styles/prism/darcula"
 import { FC } from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 
 export const Code: FC = ({ children }) => (
-  <SyntaxHighlighter language="jsx" style={darcula}>
+  <Box
+    as={SyntaxHighlighter}
+    language="jsx"
+    sh={{
+      br: 0.5,
+    }}
+    style={darcula}
+  >
     {children}
-  </SyntaxHighlighter>
+  </Box>
 )
