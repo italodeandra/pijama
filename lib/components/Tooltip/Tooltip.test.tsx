38fb066a71fa58b873b6b5ed12edb251e65f002e
Ítalo Andrade <italodeandra@gmail.com>
2021-05-20 20:01:40 +0200

    add tests

---
+++ lib/components/Tooltip/Tooltip.test.tsx
@@ -0,0 +1,100 @@
+import { render, screen } from "@testing-library/react"
+import { Button } from "../Button/Button"
+import { Tooltip } from "./Tooltip"
+import user from "@testing-library/user-event"
+
+Object.assign(window, {
+  visualViewport: { height: window.innerHeight, width: window.innerWidth },
+})
+
+describe("Tooltip", () => {
+  test("should show a tooltip when hovering", async () => {
+    render(
+      <Tooltip title="This is the tooltip">
+        <Button>Button</Button>
+      </Tooltip>
+    )
+
+    const button = screen.getByText("Button")
+
+    user.hover(button)
+
+    const tooltip = await screen.findByText("This is the tooltip")
+
+    expect(tooltip).toBeInTheDocument()
+  })
+
+  test("should show a tooltip when hovering on the top left", async () => {
+    jest
+      .spyOn(Element.prototype, "getBoundingClientRect")
+      .mockImplementation(
+        () => ({ height: 10, left: -20, top: -20, width: 10 } as any)
+      )
+
+    render(
+      <Tooltip placement="top" title="This is the tooltip">
+        <Button>Button</Button>
+      </Tooltip>
+    )
+
+    const button = screen.getByText("Button")
+
+    user.hover(button)
+
+    const tooltip = await screen.findByText("This is the tooltip")
+
+    expect(tooltip).toBeInTheDocument()
+  })
+
+  test("should show a tooltip when hovering on the bottom right", async () => {
+    jest.spyOn(Element.prototype, "getBoundingClientRect").mockImplementation(
+      () =>
+        ({
+          height: 10,
+          left: window.innerWidth,
+          top: window.innerHeight,
+          width: 10,
+        } as any)
+    )
+
+    render(
+      <Tooltip placement="bottom" title="This is the tooltip">
+        <Button>Button</Button>
+      </Tooltip>
+    )
+
+    const button = screen.getByText("Button")
+
+    user.hover(button)
+
+    const tooltip = await screen.findByText("This is the tooltip")
+
+    expect(tooltip).toBeInTheDocument()
+  })
+
+  test("should show a tooltip when hovering on the top right", async () => {
+    jest.spyOn(Element.prototype, "getBoundingClientRect").mockImplementation(
+      () =>
+        ({
+          height: 10,
+          left: window.innerWidth,
+          top: window.innerHeight + 20,
+          width: 10,
+        } as any)
+    )
+
+    render(
+      <Tooltip placement="top" title="This is the tooltip">
+        <Button>Button</Button>
+      </Tooltip>
+    )
+
+    const button = screen.getByText("Button")
+
+    user.hover(button)
+
+    const tooltip = await screen.findByText("This is the tooltip")
+
+    expect(tooltip).toBeInTheDocument()
+  })
+})
