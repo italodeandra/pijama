56b6a868187ba844977cf56b054cf32a96218597
Ítalo Andrade <italodeandra@gmail.com>
2021-05-21 13:19:54 +0200

    add tests

--- lib/styles/createTheme/createTheme.test.ts
+++ lib/styles/createTheme/createTheme.test.ts
@@ -17,4 +17,28 @@ describe("createTheme", () => {
     })
     expect(darkTheme.color.textPrimary).toBe(Gray.N100)
   })
+
+  describe("theme.transition.create", () => {
+    test("should return a string of the transition ready for css", () => {
+      const theme = createTheme()
+      const transition = theme.transition.create("top")
+      expect(transition).toBe(`top ${theme.transition.duration}ms ease`)
+    })
+
+    test("should return the same string passed to it if it's a transition already", () => {
+      const theme = createTheme()
+      const transition = theme.transition.create(
+        `top ${theme.transition.duration}ms ease` as any
+      )
+      expect(transition).toBe(`top ${theme.transition.duration}ms ease`)
+    })
+  })
+
+  describe("theme.rem", () => {
+    test("should convert pixel to rem", () => {
+      const theme = createTheme()
+      const rem = theme.rem(16)
+      expect(rem).toBe("1rem")
+    })
+  })
 })
