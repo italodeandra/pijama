b657d16697bde8c66e527e6965289b536cce0419
Ítalo Andrade <italodeandra@gmail.com>
2021-07-08 21:32:35 +0200

    add examples with start/end icon

--- lib/components/Button/Button.stories.tsx
+++ lib/components/Button/Button.stories.tsx
@@ -1,11 +1,13 @@
-import { default as ButtonComponent, ButtonProps } from "./Button"
-import type { Meta, Story } from "@storybook/react"
+import ButtonComponent, { ButtonProps } from "./Button";
+import type { Meta, Story } from "@storybook/react";
+import IconComponent from "../Icon";
+import menuIcon from "@iconify/icons-heroicons-outline/menu";
 
 const hideControl = {
   table: {
     disable: true,
   },
-}
+};
 
 // noinspection JSUnusedGlobalSymbols
 export default {
@@ -20,17 +22,35 @@ export default {
     startIcon: hideControl,
     sx: hideControl,
     theme: hideControl,
+    example: {
+      options: ["Default", "With a start icon", "With an end icon"],
+      control: { type: "radio" },
+    },
   },
   component: ButtonComponent,
   title: "Components/Button",
-} as Meta
+} as Meta;
 
-const Template: Story<ButtonProps> = (args) => <ButtonComponent {...args} />
+const Icon = () => <IconComponent icon={menuIcon} />;
 
-export const Button = Template.bind({})
+const Template: Story<ButtonProps & { example: string }> = ({
+  example,
+  ...args
+}) => {
+  switch (example) {
+    case "With a start icon":
+      return <ButtonComponent startIcon={<Icon />} {...args} />;
+    case "With an end icon":
+      return <ButtonComponent endIcon={<Icon />} {...args} />;
+    default:
+      return <ButtonComponent {...args} />;
+  }
+};
+
+export const Button = Template.bind({});
 Button.args = {
   children: "Text",
   color: "primary",
   size: "medium",
   variant: "contained",
-}
+};
