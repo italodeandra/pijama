36c462dea30868f1d0b3bcb888df5185e248534e
Ítalo Andrade <italodeandra@gmail.com>
2021-07-27 22:27:13 +0200

    fix z-index

--- lib/components/NProgress/NProgress.tsx
+++ lib/components/NProgress/NProgress.tsx
@@ -44,6 +44,7 @@ const NProgress = () => {
           top: 0,
           left: 0,
           right: 0,
+          zIndex: (theme) => theme.zIndex.appBar + 1,
         }}
       >
         <Collapse in={value !== undefined} mountOnEnter unmountOnExit>
