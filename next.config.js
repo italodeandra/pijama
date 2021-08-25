16e06ec880e31ba8e23fd1f20df9d5b204e8ab8f
Ítalo Andrade <italodeandra@gmail.com>
2021-08-25 19:43:10 +0200

    add webpack resolve aliases for removing them from the browser: `mongodb`, `mongodb-memory-server`, `socket.io`

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        mongodb: false,
+        "mongodb-memory-server": false,
+        "socket.io": false,
+      };
+    }
+    return config;
+  },
+});
