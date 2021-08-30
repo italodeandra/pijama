b450002d6d549ff7d7670c2335590bb53e9c2dbe
Ítalo Andrade <italodeandra@gmail.com>
2021-08-30 21:51:51 +0200

    add setup prop to run after the collection is synced

--- lib/next/createRepository.ts
+++ lib/next/createRepository.ts
@@ -2,17 +2,21 @@ import { Collection } from "mongodb";
 import connectToDatabase from "./connectToDatabase";
 
 export function createRepository<TSchema>(
-  collectionName: string
+  collectionName: string,
+  setup?: () => Promise<void> | void
 ): Collection<TSchema> {
   let collection: Collection<TSchema>;
 
   return new Proxy<Collection<TSchema>>({} as unknown as Collection<TSchema>, {
     get: function (target, prop) {
       return async (...props: any) => {
-        collection =
-          collection ||
-          (await connectToDatabase()).db.collection(collectionName);
-        return (collection as any)[prop](...props);
+        if (!collection) {
+          collection = (await connectToDatabase()).db.collection<TSchema>(
+            collectionName
+          );
+          await setup();
+        }
+        return collection[prop](...props);
       };
     },
   });
