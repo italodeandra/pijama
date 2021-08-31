fffb14cff8abc469fc293bed5780ac76e067a14d
Ítalo Andrade <italodeandra@gmail.com>
2021-08-31 10:55:40 +0200

    add Awaited utility type

--- lib/utils/utilityTypes.ts
+++ lib/utils/utilityTypes.ts
@@ -5,5 +5,10 @@
  * children, children of the children and so on)
  */
 export type DeepPartial<T> = {
-  [P in keyof T]?: DeepPartial<T[P]>
-}
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+/**
+ * Get the "then" argument type by unwrapping the promise.
+ */
+export type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
