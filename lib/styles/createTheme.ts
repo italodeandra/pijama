c6ccef99df300e9df0f1bcaccc30447524940d60
Ítalo Andrade <italodeandra@gmail.com>
2021-09-09 19:21:25 +0200

    add custom colors: `gray` and `lightGray`

--- lib/styles/createTheme.ts
+++ lib/styles/createTheme.ts
@@ -6,3 +6,25 @@ const createTheme: typeof muiCreateTheme = (theme) =>
   muiCreateTheme(merge(defaultThemeOptions, theme));
 
 export default createTheme;
+
+declare module "@material-ui/core/Button/Button" {
+  // noinspection JSUnusedGlobalSymbols
+  interface ButtonPropsColorOverrides {
+    lightGray: true;
+    gray: true;
+  }
+}
+
+declare module "@material-ui/core/styles/createPalette" {
+  // noinspection JSUnusedGlobalSymbols
+  interface Palette {
+    lightGray: Palette["primary"];
+    gray: Palette["primary"];
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  interface PaletteOptions {
+    lightGray?: PaletteOptions["primary"];
+    gray?: PaletteOptions["primary"];
+  }
+}
